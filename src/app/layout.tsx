17a145ab62571a2442a9044ef2adc8b3805c363f
@Li16101370 <alirazmjoo20@gmail.com>
2026-08-17 12:29:08 +0330

    fix useSearchParams by suspense in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { GlobalNavigationLoader } from "@/components/global-navigation-loader";
 import { Providers } from "./providers";
 import "./globals.css";
@@ -13,7 +14,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="fa" dir="rtl" className="font-sans">
       <body>
         <Providers>
-          <GlobalNavigationLoader />
+          <Suspense fallback={null}>
+            <GlobalNavigationLoader />
+          </Suspense>
           {children}
         </Providers>
       </body>
